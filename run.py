4e9b34c51e3408e761d93c977fc5c6c7ac68588c
Chris Zielinski <ckz8780@gmail.com>
2018-06-05 17:53:24 +0000

    Added site navigation logic

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ MONGO_USER = os.environ.get('MONGO_USER')
 MONGO_PASS = os.environ.get('MONGO_PASS')
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 app.config["MONGO_DBNAME"] = 'fitness_manager'
 app.config["MONGO_URI"] = 'mongodb://{}:{}@ds147890.mlab.com:47890/{}'.format(MONGO_USER, MONGO_PASS, app.config['MONGO_DBNAME'])
@@ -14,9 +15,23 @@ app.config["MONGO_URI"] = 'mongodb://{}:{}@ds147890.mlab.com:47890/{}'.format(MO
 mongo = PyMongo(app)
 
 @app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html")
+    
 @app.route('/routines')
 def get_routines():
     return render_template("routines.html", 
+    routines=mongo.db.routines.find())
+    
+@app.route('/workouts')
+def get_workouts():
+    return render_template("workouts.html", 
+    routines=mongo.db.routines.find())
+
+@app.route('/exercises')
+def get_exercises():
+    return render_template("exercises.html", 
     routines=mongo.db.routines.find())
     
 if __name__ == '__main__':
