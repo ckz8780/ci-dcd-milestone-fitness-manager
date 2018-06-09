9467e73a53d9a2b2f3eb83103dbf1701dc49af97
Chris Zielinski <ckz8780@gmail.com>
2018-06-09 05:22:19 +0000

    started build workout template and functionality

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, jsonify
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -19,6 +19,11 @@ mongo = PyMongo(app)
 def home():
     return render_template("home.html")
     
+
+
+
+# GET ROUTINES/WORKOUTS/EXERCISES
+
 @app.route('/routines')
 def get_routines():
     return render_template("routines.html", 
@@ -33,33 +38,35 @@ def get_workouts():
 def get_exercises():
     return render_template("exercises.html", 
     exercises=mongo.db.exercises.find())
-    
-@app.route('/exercises/add')
-def add_exercise():
-    return render_template("add_exercise.html", categories=mongo.db.exercise_categories.find())
-    
-@app.route('/workouts/build')
-def build_workout():
-    return render_template("build_workout.html")
+
+
+
+
+
+# ADD/BUILD ROUTINES/WORKOUTS/EXERCISES
     
 @app.route('/routines/build')
 def build_routine():
     return render_template("build_routine.html")
-    
-@app.route('/exercises/edit/<exercise_id>')
-def edit_exercise(exercise_id):
-    print(exercise_id)
-    return render_template("edit_exercise.html")
-    
-@app.route('/workouts/edit')
-def edit_workout():
-    return render_template("edit_workout.html")
-    
-@app.route('/routines/edit/<routine_id>')
-def edit_routine(routine_id):
-    print(routine_id)
-    return render_template("edit_routine.html")
 
+@app.route('/workouts/build')
+def build_workout():
+    return render_template("build_workout.html", categories=mongo.db.exercise_categories.find())
+
+@app.route('/exercises/add')
+def add_exercise():
+    return render_template("add_exercise.html", categories=mongo.db.exercise_categories.find())
+    
+# INSERT ROUTINES/WORKOUTS/EXERCISES
+
+@app.route('/routines/insert')
+def insert_routine():
+    return redirect(url_for('get_routines'))
+    
+@app.route('/workouts/insert')
+def insert_workout():
+    return redirect(url_for('get_workouts'))
+    
 @app.route('/exercises/insert', methods=['POST'])
 def insert_exercise():
     
@@ -73,37 +80,84 @@ def insert_exercise():
     
     inserted = mongo.db.exercises.insert_one(new_exercise)
     return redirect(url_for('get_exercises'))
+
+
+
+
+
+# EDIT ROUTINES/WORKOUTS/EXERCISES
+
+@app.route('/routines/edit/<routine_id>')
+def edit_routine(routine_id):
+    print(routine_id)
+    return render_template("edit_routine.html")
     
-@app.route('/workouts/insert')
-def insert_workout():
-    return render_template("insert_workout.html")
+@app.route('/workouts/edit/<workout_id>')
+def edit_workout(workout_id):
+    print(workout_id)
+    return render_template("edit_workout.html")
     
-@app.route('/routines/insert')
-def insert_routine():
-    return render_template("insert_routine.html")
+@app.route('/exercises/edit/<exercise_id>')
+def edit_exercise(exercise_id):
+    print(exercise_id)
+    return render_template("edit_exercise.html")
+    
+# UPDATE ROUTINES/WORKOUTS/EXERCISES  
+
+@app.route('/routines/update')
+def update_routine():
+    return redirect(url_for('get_routines'))
+
+@app.route('/workouts/update')
+def update_workout():
+    return redirect(url_for('get_workouts'))
     
 @app.route('/exercises/update')
 def update_exercise():
-    return render_template("update_exercise.html")
-    
-@app.route('/workouts/update')
-def update_workout():
-    return render_template("update_workout.html")
-    
-@app.route('/routines/update')
-def update_routine():
-    return render_template("update_routine.html")
-    
-@app.route('/exercises/delete/<exercise_id>')
-def delete_exercise(exercise_id):
-    print(exercise_id)
     return redirect(url_for('get_exercises'))
     
+    
+
+
+
+# DELETE ROUTINES/WORKOUTS/EXERCISES
+
 @app.route('/routines/delete/<routine_id>')
 def delete_routine(routine_id):
     print(routine_id)
     return redirect(url_for('get_routines'))
     
+@app.route('/workouts/delete/<workout_id>')
+def delete_workout(workout_id):
+    print(workout_id)
+    return redirect(url_for('get_workouts'))
+    
+@app.route('/exercises/delete/<exercise_id>')
+def delete_exercise(exercise_id):
+    print(exercise_id)
+    return redirect(url_for('get_exercises'))
+
+
+
+
+
+# UTILITY METHODS
+
+@app.route('/exercises/by_cat/<cat>')
+def get_exercises_by_cat(cat):
+    """Returns a list of exercises in JSON format, given a category from an AJAX request"""
+    if request.is_xhr:
+        exercise_list = [exercise for exercise in mongo.db.exercises.find({"categories": cat}, {"_id": 0})]
+        return jsonify(exercises=exercise_list)
+    return redirect(url_for('get_exercises'))
+    
+    
+    
+    
+    
+    
+    
+# RUN APP
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
