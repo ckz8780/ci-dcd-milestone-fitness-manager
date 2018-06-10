2ef67ffb33b9bb80994f628fea6919f3b2f8a612
Chris Zielinski <ckz8780@gmail.com>
2018-06-10 20:11:24 +0000

    added delete confirmations

--- static/js/main.js
+++ static/js/main.js
@@ -5,3 +5,11 @@ $('.dropdown').on('show.bs.dropdown', function() {
 $('.dropdown').on('hide.bs.dropdown', function() {
     $(this).find('.dropdown-menu').first().stop(true, true).slideUp(50);
 });
+
+$('.confirmation').on('click', function () {
+    return confirm('Are you sure?');
+});
+
+$('#confirm-delete').on('show.bs.modal', function(e) {
+    $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+});
